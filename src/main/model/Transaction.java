84c666b8f6e2dace8f21552e8cc2f92e095118fc
tb3nguye <tb3nguye@student.ubc.ca>
2025-10-05 02:02:11 -0700

    Updated Transaction class

--- src/main/model/Transaction.java
+++ src/main/model/Transaction.java
@@ -1,25 +1,57 @@
 package model;
 
-import java.time.LocalDate; // import the LocalDate class
+import java.time.LocalDateTime;
 
 /*
- * Represents a record of a single stock transaction (buy or sell).
+ * Represents a history record of a single stock transaction (buy or sell).
  * Each Transaction object includes details such as the date, stock symbol,
  * action type (BUY/SELL), number of shares, price per share, and total amount.
  * This class is immutable once created and used for maintaining transaction history.
  */
 public class Transaction {
-    private LocalDate date;
+    private LocalDateTime date;
+    private String symbol;
+    private String action;
+    private double shares;
+    private double price;
+    private double total;
 
-
-    public Transaction() {
+    // EFFECTS: constructs a Transaction with dateTime is set to LocalDateTime.now(), symbol, action, shares, price, total of 
+    // a recent action (BUY/SELL) of a stock.
+    public Transaction(LocalDateTime dateTime, String symbol, String action, double shares, double price, double total) {
         // stub
     }
 
-    // EFFECTS: display a record of a single stock transaction (buy or sell) in the console
-    // with the format as 
+    // getters
+    public LocalDateTime getDate() {
+        return null; // stub
+    }
+
+    public String getSymbol() {
+        return null; // stub
+    }
+
+    public String getAction() {
+        return null; // stub
+    }
+
+    public double getShares() {
+        return 0.0; // stub
+    }
+
+    public double getPrice() {
+        return 0.0; // stub
+    }
+
+    public double getTotal() {
+        return 0.0; // stub
+    }
+
+    @Override
+    // EFFECTS: display a record of a single stock transaction (BUY/ SELL) in the console
+    // with the format as below:
     // | Date | Symbol | Action | Shares | Price | Total |
-    public void printTransaction() {
-        // stub
+    public String toString() {
+        return null; // stub
     }
 }
