a19f702d4959c8022929343e0c33ee85f91bb223
tb3nguye <tb3nguye@student.ubc.ca>
2025-10-07 20:48:38 -0700

    Edited tests for TestStock class

--- src/test/model/TestStock.java
+++ src/test/model/TestStock.java
@@ -1,5 +1,6 @@
 package model;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,71 +11,75 @@ public class TestStock {
     private Stock amazon;
     private Stock google;
     private Stock nvidia;
-    private Stock nvidiaWithoutShares;
     private Stock costco;
     private Stock jpmorgan;
     
     @BeforeEach
     void runBefore() {
         // constructor with shares parameter
-        apple = new Stock("AAPL", "Apple Inc.", "Technology", 10, 258.02);
-        amazon = new Stock("AMZN", "Amazon Com Inc.", "Consumer Discretionary", 5, 219.51);
-        google = new Stock("GOOGL", "Alphabet Inc.", "Technology", 8, 246.45);
-        nvidia = new Stock("NVDA", "Nvidia Corp", "Technology", 10, 187.55);
-        costco = new Stock("COST", "Costco Wholesale Corp.", "Consumer Staples", 4, 915.95);
-        jpmorgan = new Stock("JPM", "JPMorgan Chase & Corp", "Financial Services", 10, 310.35);
-
-        // constructor without shares parameter
-        nvidiaWithoutShares = new Stock("NVDA", "Nvidia Corp", "Technology", 187.55);
+        apple = new Stock("AAPL", "Apple Inc.", "Technology", 258.02);
+        amazon = new Stock("AMZN", "Amazon Com Inc.", "Consumer Discretionary", 219.51);
+        google = new Stock("GOOGL", "Alphabet Inc.", "Technology", 246.45);
+        nvidia = new Stock("NVDA", "Nvidia Corp", "Technology", 187.55);
+        costco = new Stock("COST", "Costco Wholesale Corp.", "Consumer Staples",915.95);
+        jpmorgan = new Stock("JPM", "JPMorgan Chase & Corp", "Financial Services", 310.35);
     }
 
     @Test
-    void testConstrutorWithShares() {
+    void testConstrutor() {
         // test JPMorgan stock
         assertEquals("JPM", jpmorgan.getSymbol());
         assertEquals("JPMorgan Chase & Corp", jpmorgan.getCompanyName());
         assertEquals("Financial Services", jpmorgan.getSector());
-        assertEquals(10, jpmorgan.getSharesOwned());
-        assertEquals(310.35, jpmorgan.getStockPrice());
-        assertEquals(10 * 310.35, jpmorgan.getMarketValue());
+        assertEquals(310.35, jpmorgan.getCurrentPrice());
+
+        // assertEquals(10, jpmorgan.getSharesOwned());
+        // assertEquals(10 * 310.35, jpmorgan.getMarketValue());
+    }
+
+    // @Test
+    // void testConstrutorWithoutShares() {
+    //     // test NVDA stock
+    //     assertEquals("NVDA", nvidiaWithoutShares.getSymbol());
+    //     assertEquals("Nvidia Corp", nvidiaWithoutShares.getCompanyName());
+    //     assertEquals("Technology", nvidiaWithoutShares.getSector());
+    //     assertEquals(0, nvidiaWithoutShares.getSharesOwned());
+    //     assertEquals(187.55, nvidiaWithoutShares.getStockPrice());
+    //     assertEquals(0 * 187.55, nvidiaWithoutShares.getMarketValue());
+    // }
+
+    @Test
+    void testUpdateCurrentPrice() {
+        costco.updateCurrentPrice(1500.55);
+        assertEquals(1500.55, costco.getCurrentPrice());
+
+        // assertEquals(1500.55 * 4, costco.getMarketValue());
     }
 
     @Test
-    void testConstrutorWithoutShares() {
-        // test NVDA stock
-        assertEquals("NVDA", nvidiaWithoutShares.getSymbol());
-        assertEquals("Nvidia Corp", nvidiaWithoutShares.getCompanyName());
-        assertEquals("Technology", nvidiaWithoutShares.getSector());
-        assertEquals(0, nvidiaWithoutShares.getSharesOwned());
-        assertEquals(187.55, nvidiaWithoutShares.getStockPrice());
-        assertEquals(0 * 187.55, nvidiaWithoutShares.getMarketValue());
+    void testPrintStock() {
+        String stockString = "| NVDA | Nvidia Corp | Technology | 187.55 |";
+        assertTrue(stockString.equals(nvidia.stockToString()));
     }
 
-    @Test
-    void testUpdateStockPrice() {
-        costco.updateStockPrice(1500.55);
-        assertEquals(1500.55, costco.getStockPrice());
-        assertEquals(1500.55 * 4, costco.getMarketValue());
-    }
+    // @Test
+    // void testBuyShares() {
+    //     costco.buyShares(20, 105.55);
+    //     assertEquals(4 + 20, costco.getSharesOwned());
+    // }
 
-    @Test
-    void testBuyShares() {
-        costco.buyShares(20, 105.55);
-        assertEquals(4 + 20, costco.getSharesOwned());
-    }
+    // @Test
+    // void testSellShares() {
+    //     costco.updateStockPrice(1500.55);
+    //     costco.sellShares(1);
+    //     assertEquals(4 - 1, costco.getSharesOwned());
+    //     assertEquals(1500.55 * 4 - 1500.55 * 1, costco.getMarketValue());
+    // }    
 
-    @Test
-    void testSellShares() {
-        costco.updateStockPrice(1500.55);
-        costco.sellShares(1);
-        assertEquals(4 - 1, costco.getSharesOwned());
-        assertEquals(1500.55 * 4 - 1500.55 * 1, costco.getMarketValue());
-    }    
-
-    @Test
-    void testSellSharesWhenSellSharesMoreThanCurrentShares() {
-        costco.sellShares(5);
-        assertEquals(4, costco.getSharesOwned());
-        assertEquals(915.95 * 4, costco.getMarketValue());
-    } 
+    // @Test
+    // void testSellSharesWhenSellSharesMoreThanCurrentShares() {
+    //     costco.sellShares(5);
+    //     assertEquals(4, costco.getSharesOwned());
+    //     assertEquals(915.95 * 4, costco.getMarketValue());
+    // } 
 }
