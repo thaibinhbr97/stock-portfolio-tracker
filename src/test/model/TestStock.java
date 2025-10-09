eec9ad4f5cd73acdb5caf84c755c48c39b695167
tb3nguye <tb3nguye@student.ubc.ca>
2025-10-09 00:12:21 -0700

    Modified tests for TestStock class

--- src/test/model/TestStock.java
+++ src/test/model/TestStock.java
@@ -32,54 +32,17 @@ public class TestStock {
         assertEquals("JPMorgan Chase & Corp", jpmorgan.getCompanyName());
         assertEquals("Financial Services", jpmorgan.getSector());
         assertEquals(310.35, jpmorgan.getCurrentPrice());
-
-        // assertEquals(10, jpmorgan.getSharesOwned());
-        // assertEquals(10 * 310.35, jpmorgan.getMarketValue());
     }
-
-    // @Test
-    // void testConstrutorWithoutShares() {
-    //     // test NVDA stock
-    //     assertEquals("NVDA", nvidiaWithoutShares.getSymbol());
-    //     assertEquals("Nvidia Corp", nvidiaWithoutShares.getCompanyName());
-    //     assertEquals("Technology", nvidiaWithoutShares.getSector());
-    //     assertEquals(0, nvidiaWithoutShares.getSharesOwned());
-    //     assertEquals(187.55, nvidiaWithoutShares.getStockPrice());
-    //     assertEquals(0 * 187.55, nvidiaWithoutShares.getMarketValue());
-    // }
 
     @Test
     void testUpdateCurrentPrice() {
         costco.updateCurrentPrice(1500.55);
         assertEquals(1500.55, costco.getCurrentPrice());
-
-        // assertEquals(1500.55 * 4, costco.getMarketValue());
     }
 
     @Test
-    void testPrintStock() {
+    void testStockToString() {
         String stockString = "| NVDA | Nvidia Corp | Technology | 187.55 |";
-        assertTrue(stockString.equals(nvidia.stockToString()));
+        assertTrue(stockString.equals(nvidia.toString()));
     }
-
-    // @Test
-    // void testBuyShares() {
-    //     costco.buyShares(20, 105.55);
-    //     assertEquals(4 + 20, costco.getSharesOwned());
-    // }
-
-    // @Test
-    // void testSellShares() {
-    //     costco.updateStockPrice(1500.55);
-    //     costco.sellShares(1);
-    //     assertEquals(4 - 1, costco.getSharesOwned());
-    //     assertEquals(1500.55 * 4 - 1500.55 * 1, costco.getMarketValue());
-    // }    
-
-    // @Test
-    // void testSellSharesWhenSellSharesMoreThanCurrentShares() {
-    //     costco.sellShares(5);
-    //     assertEquals(4, costco.getSharesOwned());
-    //     assertEquals(915.95 * 4, costco.getMarketValue());
-    // } 
 }
