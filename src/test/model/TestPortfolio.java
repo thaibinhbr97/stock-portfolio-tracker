d74c521dcc9250a49c65626c464138ac40767eba
tb3nguye <tb3nguye@student.ubc.ca>
2025-10-09 00:11:43 -0700

    Edited tests for TestPortfolio class

--- src/test/model/TestPortfolio.java
+++ src/test/model/TestPortfolio.java
@@ -1,20 +1,24 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 public class TestPortfolio {
-    // symbols string
+    // symbol strings
     private static final String ORCL = "ORCL";
+    private static final String APPL = "AAPL";
     private static final String AMZN = "AMZN";
-
+    private static final String GOOGL = "GOOGL";
+    private static final String NVDA = "NVDA";
+    private static final String COST = "COST";
+    private static final String JPM = "JPM";
+    
     private Stock apple;
     private Stock amazon;
     private Stock google;
@@ -22,99 +26,91 @@ public class TestPortfolio {
     private Stock costco;
     private Stock jpmorgan;
 
-    private List<Stock> testStocks;
-    private double testTotalValue;
-    private LocalDateTime testLastUpdated;
-    private List<String> testSymbols;
-
     private Portfolio portfolio;
-
+    private LocalDateTime lastUpdated;
     
     @BeforeEach
     void runBefore() {
         // constructor with shares parameter
-        apple = new Stock("AAPL", "Apple Inc.", "Technology", 0, 258.02);
-        amazon = new Stock("AMZN", "Amazon Com Inc.", "Consumer Discretionary",0, 219.51);
-        google = new Stock("GOOGL", "Alphabet Inc.", "Technology",0, 246.45);
-        nvidia = new Stock("NVDA", "Nvidia Corp", "Technology", 0, 187.55);
-        costco = new Stock("COST", "Costco Wholesale Corp.", "Consumer Staples",0, 915.95);
-        jpmorgan = new Stock("JPM", "JPMorgan Chase & Corp", "Financial Services", 0, 310.35);
+        apple = new Stock("AAPL", "Apple Inc.", "Technology",250.00);
+        amazon = new Stock("AMZN", "Amazon Com Inc.", "Consumer Discretionary",200.00);
+        google = new Stock("GOOGL", "Alphabet Inc.", "Technology",150.00);
+        nvidia = new Stock("NVDA", "Nvidia Corp", "Technology",150.00);
+        costco = new Stock("COST", "Costco Wholesale Corp.", "Consumer Staples",450.00);
+        jpmorgan = new Stock("JPM", "JPMorgan Chase & Corp", "Financial Services",300.00);
 
-        testStocks = new ArrayList<>();
-        testStocks.add(apple);
-        testStocks.add(amazon);
-        testStocks.add(google);
-        testStocks.add(nvidia);
-        testStocks.add(costco);
-        testStocks.add(jpmorgan);
-        testLastUpdated = LocalDateTime.now();
-        portfolio = new Portfolio("Thai Binh Nguyen", 10000.00, testStocks, 14292.15, testLastUpdated);
+        LocalDateTime lastUpdated = LocalDateTime.of(2025, 10, 5, 6, 23, 32);
+        portfolio = new Portfolio("Brad", 10000.00, lastUpdated);
     }
 
     @Test
-    void testGetStock() {
-        assertEquals(null, portfolio.getStock(ORCL));
-        assertEquals(amazon, portfolio.getStock(AMZN));
+    void testConstrutor() {
+        assertEquals("Brad", portfolio.getOwner());
+        assertEquals(10000.00, portfolio.getCashBalance());
+        assertEquals(0.0, portfolio.getPortfolioValue());
+        assertEquals(LocalDateTime.of(2025, 10, 5, 6, 23, 32), portfolio.getLastUpdated());
+        assertEquals(0, portfolio.getHoldings().size());
     }
 
     @Test
-    void testAddStockWhenStockNotExist() {
-        assertFalse(testSymbols.contains(ORCL));
-        Stock oracle = new Stock("ORCL", "Oracle Corp", "Technology", 0, 258.02);
-        assertEquals(0 , oracle.getSharesOwned());
-        assertEquals(, oracle);, testStocks.getTotalValue());
-    }
-
-
-
-    @Test
-    void testConstrutorWithShares() {
-        // test JPMorgan stock
-        assertEquals("JPM", jpmorgan.getSymbol());
-        assertEquals("JPMorgan Chase & Corp", jpmorgan.getCompanyName());
-        assertEquals("Financial Services", jpmorgan.getSector());
-        assertEquals(10, jpmorgan.getSharesOwned());
-        assertEquals(310.35, jpmorgan.getPrice());
-        assertEquals(10 * 310.35, jpmorgan.getMarketValue());
+    void testBuyShareWhichNotInPortfolio() {
+        assertFalse(portfolio.getHoldings().containsKey(NVDA));
+        portfolio.buyShare(NVDA, 3.0);
+        assertEquals(10000.0 - 150.0 * 3.0, portfolio.getCashBalance());
+        assertEquals(150.0 * 3.0, portfolio.getPortfolioValue());
+        assertTrue(portfolio.getHoldings().containsKey(NVDA));
     }
 
     @Test
-    void testConstrutorWithoutShares() {
-        // test JPMorgan stock
-        assertEquals("NVDA", nvidiaWithoutShares.getSymbol());
-        assertEquals("Nvidia Corp", nvidiaWithoutShares.getCompanyName());
-        assertEquals("Technology", nvidiaWithoutShares.getSector());
-        assertEquals(0, nvidiaWithoutShares.getSharesOwned());
-        assertEquals(187.55, nvidiaWithoutShares.getPrice());
-        assertEquals(0 * 187.55, nvidiaWithoutShares.getMarketValue());
+    void testBuyShareWhichInPortfolio() {
+        portfolio.buyShare(NVDA, 3.0);
+        assertTrue(portfolio.getHoldings().containsKey(NVDA));
+        portfolio.buyShare(NVDA, 3.0);
+        assertTrue(portfolio.getHoldings().containsKey(NVDA));
+        assertEquals(10000.0 - 150.0 * 6.0, portfolio.getCashBalance());
+        assertEquals(150.0 * 6.0, portfolio.getPortfolioValue());
+        assertTrue(portfolio.getHoldings().containsKey(NVDA));
+    }   
+    
+    @Test
+    void testBuyShareWhichExceedCashBalanace() {
+        assertFalse(portfolio.getHoldings().containsKey(NVDA));
+        portfolio.buyShare(NVDA, 100.0);
+        assertFalse(portfolio.getHoldings().containsKey(NVDA));
+        assertEquals(10000.0, portfolio.getCashBalance());
+        assertEquals(0.0, portfolio.getPortfolioValue());
     }
 
     @Test
-    void testUpdateStockPrice() {
-        costco.updateStockPrice(1500.55);
-        assertEquals(915.95, costco.getAveragePurchasePrice());
-        assertEquals(1500.55 * 4, costco.getMarketValue());
+    void testSellAllShareForOneHolding() {
+        portfolio.buyShare(NVDA, 10.0);
+        assertTrue(portfolio.getHoldings().containsKey(NVDA));
+        assertEquals(10000.0 - 150.0 * 10.0, portfolio.getCashBalance());
+        assertEquals(150.0 * 10.0, portfolio.getPortfolioValue());
+        portfolio.sellShare(NVDA, 10.0);
+        assertFalse(portfolio.getHoldings().containsKey(NVDA));        
+        assertEquals(10000.0, portfolio.getCashBalance());
+        assertEquals(0.0, portfolio.getPortfolioValue());
     }
 
     @Test
-    void testBuyShares() {
-        costco.buyShares(20, 105.55);
-        assertEquals(4 + 20, costco.getSharesOwned());
-        assertEquals((915.95 * 4 + 105.55 * 20) / 2, costco.getAveragePurchasePrice());
+    void testSellSomeShareForOneHolding() {
+        portfolio.buyShare(NVDA, 10.0);
+        assertTrue(portfolio.getHoldings().containsKey(NVDA));
+        assertEquals(10000.0 - 150.0 * 10.0, portfolio.getCashBalance());
+        assertEquals(150.0 * 10.0, portfolio.getPortfolioValue());
+        portfolio.sellShare(NVDA, 5.0);
+        assertTrue(portfolio.getHoldings().containsKey(NVDA));        
+        assertEquals(10000.0 - 150.0 * 5.0, portfolio.getCashBalance());
+        assertEquals(150.0 * 5.0, portfolio.getPortfolioValue());
     }
 
     @Test
-    void testSellShares() {
-        costco.updateStockPrice(1500.55);
-        costco.sellShares(1);
-        assertEquals(4 - 1, costco.getSharesOwned());
-        assertEquals(1500.55 * 4 - 1500.55 * 1, costco.getMarketValue());
-    }    
-
-    @Test
-    void testSellSharesWhenSellSharesMoreThanCurrentShares() {
-        costco.sellShares(5);
-        assertEquals(4, costco.getSharesOwned());
-        assertEquals(915.95 * 4, costco.getMarketValue());
-    } 
+    void testPortfolioToString() {
+        portfolio.buyShare(NVDA, 3.0);
+        String portfolioString = "Owner name: Brad\n";
+        portfolioString += "Cash Balance: $10000\n";
+        portfolioString += "| Symbol | CurrentPrice | AveragePrice | Shares | MarketValue | Profit/Loss |\n";
+        portfolioString += "| NVDA | $150 | $150 | 3.0 | $450.00 | +$0 |\n";
+    }
 }
