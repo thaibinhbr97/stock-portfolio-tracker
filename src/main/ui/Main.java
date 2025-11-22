f1eeb022e5a87f6e26a7b3a43b1322ca582f781a
tb3nguye <tb3nguye@student.ubc.ca>
2025-11-21 16:20:12 -0800

    updated main to include slashscreen upon start

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,10 +3,22 @@ package ui;
 import javax.swing.SwingUtilities;
 
 import ui.gui.MainFrame;
+import ui.gui.SplashScreen;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // new StockPortfolioApp();
+    public static void main(String[] args) {
+        SplashScreen splash = new SplashScreen();
+        splash.setVisible(true);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        splash.setVisible(false);
+        splash.dispose();
+
         SwingUtilities.invokeLater(MainFrame::new);
     }
 }
